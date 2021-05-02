c5d75673489c376474f32f67ee4cb41ae0be1846
aattanasio91 <agustin.attanasio@gmail.com>
2021-05-02 17:39:17 -0300

    Primer requerimiento completo con tests

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,19 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+//TODO: Agregar interface Barrio de la misma forma que con los estados
+//TODO: Agregar el barrio al constructor del cliente
+//TODO: Agregar la variacion por barrio al metodo de darPropina
+//TODO: Agregar tests y/o modificar los que estan
+
+class Cliente(var dineroEnBolsillo : Double, var estadoDeAnimo: EstadosDeAnimo) {
+
+    var importePedido : Double = 0.0
+
+    fun realizarPedido(importe : Double) {
+        this.importePedido = importe
+    }
+
+    fun darPropina(importePedido : Double, dineroEnBolsillo: Double, estadoDeAnimo: EstadosDeAnimo) =
+        estadoDeAnimo.aplicarEstadoAnimo(importePedido, dineroEnBolsillo)
+
 }
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -1,8 +1,33 @@
 package ar.edu.unahur.obj2.socios
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class ClienteTest : DescribeSpec({
   describe("Un/a cliente") {
+    it("Feliz"){
+      val agustin = Cliente(1500.0, Feliz)
+      agustin.realizarPedido(1000.0)
+      agustin.darPropina(agustin.importePedido, agustin.dineroEnBolsillo, agustin.estadoDeAnimo)
+        .shouldBe(250)
+    }
+    it("Enojado"){
+      val agustin = Cliente(1500.0, Enojado)
+      agustin.realizarPedido(1000.0)
+      agustin.darPropina(agustin.importePedido, agustin.dineroEnBolsillo, agustin.estadoDeAnimo)
+        .shouldBe(0)
+    }
+    it("Indiferente") {
+      val agustin = Cliente(1500.0, Indiferente)
+      agustin.realizarPedido(1000.0)
+      agustin.darPropina(agustin.importePedido, agustin.dineroEnBolsillo, agustin.estadoDeAnimo)
+        .shouldBe(1500)
+    }
+    it("Resfriado"){
+      val agustin = Cliente(1500.0, Resfriado)
+      agustin.realizarPedido(1000.0)
+      agustin.darPropina(agustin.importePedido, agustin.dineroEnBolsillo, agustin.estadoDeAnimo)
+        .shouldBe(1000)
+    }
   }
 })
